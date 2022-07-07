5379dc867a185f760df2a690cbafd092d749dd89
Saraswati Kondikoppa <67637467+saraswati54@users.noreply.github.com>
2022-07-07 15:46:55 +0530

    Add files via upload

---
+++ Mod1Assign1.py
@@ -0,0 +1,39 @@
+# Assignment1 
+# Ans1
+string = "Discover, Learning, with, Edureka"
+
+print("number of lowercase letters a in string :" )
+print(string.count('a'))
+print(string.count('o'))
+
+print("number of uppercase letters a in string :" )
+print(string.count('L'))
+print(string.count('N'))
+
+
+
+#Ans3
+print(type(0X7AE))
+print(type(3+4j))
+#print(type(-01234))
+print(type(3.14e-2))
+
+#Ans4
+var = '27' #as string
+
+#%i -  Signed Decimal Integer
+print('Variable as signed decimal integer = %i' %(int(var)))
+
+#%o - Octal Integer
+print('Variable as octal integer = %o' %(int(var)))
+
+#%x - Hexa Decimal Integer
+print('Variable as Hexa Decimal Integer = %X' %(int(var)))
+
+#%f - Float
+print('Variable as Floating Point Real Num = %f' %(float(var)))
+
+#%x - Exonential Notation
+print('Variable as Exonential Notation = %e' %(int(var)))
+
+#character, signed decimal integer, octal integer, hexa decimal integerUpper, FloatingPoint real num , exponential
